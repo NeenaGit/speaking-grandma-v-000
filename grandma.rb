2f11e4ac1a25ad9411cb0bc56b1d74981b0e8cb9
Neena van de Water <nkuriakose@hotmail.com>
2017-10-30 17:11:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    # puts "I LOVE YOU TOO PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif string.upcase
-    # puts "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   else
-    #  puts "HUH?! SPEAK UP, SONNY!"
+     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
 
