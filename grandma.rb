ab2fafbd55010733391875bfeb2b9e0b0f34c3da
Neena van de Water <nkuriakose@hotmail.com>
2017-10-30 17:28:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif string.downcase
+  elsif string==string.downcase
     puts "HUH?! SPEAK UP, SONNY!"
-  else string.upcase
+  else 
      puts "NO, NOT SINCE 1938!"
   end
 end
